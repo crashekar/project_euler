f4af3c5ce6a18daf88e7b9ca6fa142c0d7b08173
Chandrashekar Jayaraman <jchandrashekar@zeomega.com>
2013-03-01 19:35:38 +0530

    Solved pythagorean triplet

--- 9.py
+++ 9.py
@@ -1,6 +1,6 @@
-for i in range(1,99):
-    for j in range(1,99):
-        for k in range(1,99):
-            if i*i + j*j == k*k and i + j + k == 1000:
+for i in range(1,500):
+    for j in range(1,i):
+        for k in range(1,j):
+            if k*k + j*j == i*i and i + j + k == 1000:
                 print "success %s,%s, %s" % (i,j,k)
         
